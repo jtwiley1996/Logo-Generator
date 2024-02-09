b00ff10a8f7dd398e3734b55f1e2939fd73fcb37
jtwiley1996 <98716163+jtwiley1996@users.noreply.github.com>
2024-02-08 18:21:45 -0600

    added code to shape.js

---
+++ library/shape.js
@@ -0,0 +1,39 @@
+const colors = require('./colors');
+
+class shapes {
+    constructor(acronym, shapeColor, textColor){
+        textColor = textColor.toLowerCase();
+        shapeColor = shapeColor.toLowerCase();
+
+        const hexRegex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/;
+        if (acronym.length > 3) {
+            throw new Error('3 characters or less');
+        }
+        if (!(hexRegex.test(textColor) || colors[textColor.toLowerCase()])){
+            throw new Error('invalid input: must be valid hex code or color name')
+        }
+        if (!(hexRegex.test(shapeColor) || colors[shapeColor.toLowerCase()])){
+            throw new Error('invalid input: must be valid hex code or color name')
+        }
+
+        this.acronym = acronym.toUpperCase();
+
+        if (hexRegex.test(textColor)) {
+            this.textColor = textColor;
+        } else {
+            this.textColor = colors[textColor];
+        }
+        
+        if (hexRegex.test(shapeColor)) {
+            this.shapeColor = shapeColor;
+        } else {
+            this.shapeColor = colors[shapeColorColor];
+        }
+    }
+    render(){
+        const svgData = this.generateSVG();
+        return svgData;
+    }
+}
+
+module.exports = shapes;
